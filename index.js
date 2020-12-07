8bfc033bac812c7e498e6a90de8376abf15bbfbb
GustavoDell <gustavo.dell@outlook.com>
2020-12-07 17:18:39 -0300

    02.Adicionando comportamentos

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 class Cliente{
     nome;
     cpf;
+}
+
+class ContaCorrenta{
     agencia;
-    saldo;
+    _saldo = 0;
+    
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+           
+    }
+    
+    depositar(valor){
+        if(valor <= 0){
+            return; //early return caso a condição for verdadeira ele já sai da execulção da função por ter o return
+        }  
+        this._saldo += valor;  
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrenta();
+contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteAlice = new ContaCorrenta();
+
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
